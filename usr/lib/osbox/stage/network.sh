10f234b35921137b065786e301e1b89c057d53aa
jerryhopper <hopper.jerry@gmail.com>
2020-03-12 21:52:23 +0100

    ..

--- usr/lib/osbox/stage/network.sh
+++ usr/lib/osbox/stage/network.sh
@@ -2,38 +2,36 @@
 
 
 
-if [ "$1" == "info" ]; then
-    bash /usr/lib/osbox/stage/networkinfo.sh
-    exit 0
-fi
-if [ "$1" == "scan" ]; then
-    bash /usr/lib/osbox/stage/networkscan.sh
-    exit 0
-fi
-
-if [ "$1" == "reset" ]; then
-    #echo "network reset"
-    bash /usr/lib/osbox/stage/networkreset.sh
-    exit 0
-fi
-if [ "$1" == "set" ]; then
-    echo "networksetstatic.sh $2 $3 $4"
-    bash /usr/lib/osbox/stage/networksetstatic.sh $2 $3 $4
-    exit 0
-    sleep 1
-fi
-
-if [ "$1" == "current" ]; then
-    #echo "network current"
-    bash /usr/lib/osbox/stage/networkcurrent.sh
-    exit 0
-fi
 
 
+  if [ "$1" == "info" ]; then
+      bash /usr/lib/osbox/stage/networkinfo.sh
+      exit 0
+  fi
+  
+  if [ "$1" == "scan" ]; then
+      bash /usr/lib/osbox/stage/networkscan.sh
+      exit 0
+  fi
 
+  if [ "$1" == "reset" ]; then
+      #echo "network reset"
+      bash /usr/lib/osbox/stage/networkreset.sh
+      exit 0
+  fi
 
+  if [ "$1" == "set" ]; then
+      echo "networksetstatic.sh $2 $3 $4 $5 $6"
+      bash /usr/lib/osbox/stage/networksetstatic.sh $2 $3 $4 $5 $6
+      exit 0
+      sleep 1
+  fi
 
-
+  if [ "$1" == "current" ]; then
+      #echo "network current"
+      bash /usr/lib/osbox/stage/networkcurrent.sh
+      exit 0
+  fi
 
 
 
