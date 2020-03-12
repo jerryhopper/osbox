ef4834d79864e33973a753770f2c562814521ad6
jerryhopper <hopper.jerry@gmail.com>
2020-03-12 01:26:24 +0100

    ..

--- usr/lib/osbox/stage/boot_command.sh
+++ usr/lib/osbox/stage/boot_command.sh
@@ -1,69 +1,101 @@
 #!/bin/bash
 
+#########################################################################3
 source /usr/share/osbox/variables
 source /usr/lib/osbox/func/is_command
 source /usr/lib/osbox/func/make_repo
+source /usr/lib/osbox/func/minfo
+source /usr/lib/osbox/func/install_osboxweb
+#########################################################################3
+
+
+
+# Check if statefile exists.
+if [ ! -f $OSBOX_STATE_FILE ]; then
+  mkdir $OSBOX_ETC
+  echo "0">$OSBOX_STATE_FILE
+  OSBOX_STATE="0"
+else
+  OSBOX_STATE=$(<$OSBOX_STATE_FILE)
+fi
 
 
 
 
-OSBOX_ID=${$OSBOX_ID_FILE}
-OSBOX_STATE=${$OSBOX_STATE_FILE}
+# run through the installation stages...
 
 
 
+
+# hardware detection
 if [ "$OSBOX_STATE" == "0" ]; then
+  echo "State = 0"
+  echo $(minfo)>$OSBOX_HARDWARE
+  # Set state.
+  echo "1">$OSBOX_STATE_FILE
+  OSBOX_STATE="1"
+fi
+
+
+
+
+# doublecheck for git availability
+if [ "$OSBOX_STATE" == "1" ]; then
+  echo "State = 1"
     if $(is_command git) ; then
-        echo  "git is available."
-        echo "1">$OSBOX_STATE_FILE
-        OSBOX_STATE = "1"
+        #echo  "git is available."
+        # Set state.
+        echo "2" > $OSBOX_STATE_FILE
+        OSBOX_STATE="2"
     else
         apt install git -y
-        #/boot/dietpi/dietpi-software install 17 --unattended
-        #echo 0 > /sys/class/leds/nanopi:red:pwr/brightness
-        #echo 0 > /sys/class/leds/nanopi\:green\:status/brightness
-        sleep 1
-        #echo 1 > /sys/class/leds/nanopi\:green\:status/brightness
-        reboot
-    fi
-
-fi
-
-
-
-if [ "$OSBOX_STATE" == "1" ]; then
-    if $(is_command lighttpd) ; then
-        echo  "lighttpd is installed."
-        # is blackbox installed?
-        if [[ ! -d "/var/www/html/blackbox" ]] ; then
-            echo "no blackbox webdir."
-            mkdir -p /var/www/html/blackbox
-            make_repo /var/www/html/blackbox https://github.com/jerryhopper/blackboxweb.git
-            #echo "heartbeat" > /sys/class/leds/nanopi:red:pwr/trigger
-            echo "2">$OSBOX_STATE_FILE
-            OSBOX_STATE = "2"
-        fi
-
-    else
-        echo "install started : lighttpd "
-        /boot/dietpi/dietpi-software install 81 --unattended
-        #apt install -y git
-        echo "install finished : lighttpd">>/boot/log.txt
         sleep 1
         reboot
-
     fi
 fi
 
+
+
+# install prerequisites
 if [ "$OSBOX_STATE" == "2" ]; then
-    # git lighttpd
-    apt-get -y install php-common php-sqlite3 php-xml php-intl php-zip php-mbstring php-gd php-apcu php-cgi composer dialog dhcpcd5 dnsutils lsof nmap netcat idn2 dns-root-data composer
-    echo "3">$OSBOX_STATE_FILE
+    echo "State = 2"
+    apt-get -y install php-common php-sqlite3 php-xml php-intl php-zip php-mbstring php-gd php-apcu php-cgi composer dialog dhcpcd5 dnsutils lsof nmap netcat idn2 dns-root-data >/dev/null
+
+    # Set state.
+    echo "3" > $OSBOX_STATE_FILE
     OSBOX_STATE=3
+fi
+
+
+
+
+
+# install the osbox-web
+if [ "$OSBOX_STATE" == "3" ]; then
+    echo "State = 3"
+    #telegram "INSTALLSTATE=$OSBOX_STATE Cloning blackbox.git"
+    install_osboxweb
+    # Set state.
+    echo "4" > $OSBOX_STATE_FILE
+    OSBOX_STATE=4
 
 fi
 
-if [ "$OSBOX_STATE" == "3" ]; then
+
+# install the osbox-web
+if [ "$OSBOX_STATE" == "4" ]; then
+    #
+
+fi
+
+
+
+
+
+
+
+
+if [ "$OSBOX_STATE" == "X" ]; then
         # is pihole installed?
 
         #copy_piholeftlconf
