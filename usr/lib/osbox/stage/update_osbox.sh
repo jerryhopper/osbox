32dff7603ce3ee7d20f63b0c69f0a4bd81b5cd2a
jerryhopper <hopper.jerry@gmail.com>
2020-03-12 12:01:00 +0100

    Update update_osbox.sh

--- usr/lib/osbox/stage/update_osbox.sh
+++ usr/lib/osbox/stage/update_osbox.sh
@@ -6,9 +6,15 @@
 source /usr/share/osbox/variables
 source /usr/lib/osbox/func/is_command
 source /usr/lib/osbox/func/make_repo
+source /usr/lib/osbox/func/update_repo
 source /usr/lib/osbox/func/minfo
 source /usr/lib/osbox/func/install_osboxweb
 #########################################################################3
 
 
-make_repo /usr/local/src/osbox https://github.com/jerryhopper/osbox.git
+echo "Updateing OSBox core."
+
+update_repo /usr/local/src/osbox
+
+#make_repo /usr/local/src/osbox https://github.com/jerryhopper/osbox.git
+
