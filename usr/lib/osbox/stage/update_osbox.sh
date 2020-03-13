fe3a0f8084588a6dd79b78335048441b93b14201
jerryhopper <hopper.jerry@gmail.com>
2020-03-13 13:21:18 +0100

    Update update_osbox.sh

--- usr/lib/osbox/stage/update_osbox.sh
+++ usr/lib/osbox/stage/update_osbox.sh
@@ -5,6 +5,7 @@
 #########################################################################3
 source /usr/share/osbox/variables
 source /usr/lib/osbox/func/is_command
+source /usr/lib/osbox/func/is_repo
 source /usr/lib/osbox/func/make_repo
 source /usr/lib/osbox/func/update_repo
 source /usr/lib/osbox/func/getgitfiles
