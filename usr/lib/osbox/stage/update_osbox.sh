a9b519b2c269fec850850c7067c186e68e859896
jerryhopper <hopper.jerry@gmail.com>
2020-03-13 13:20:12 +0100

    ..

--- usr/lib/osbox/stage/update_osbox.sh
+++ usr/lib/osbox/stage/update_osbox.sh
@@ -7,6 +7,7 @@ source /usr/share/osbox/variables
 source /usr/lib/osbox/func/is_command
 source /usr/lib/osbox/func/make_repo
 source /usr/lib/osbox/func/update_repo
+source /usr/lib/osbox/func/getgitfiles
 source /usr/lib/osbox/func/minfo
 source /usr/lib/osbox/func/install_osboxweb
 #########################################################################3
@@ -20,13 +21,13 @@ echo "Current state : $(<$OSBOX_STATE_FILE)"
 echo ". "
 echo "Updateing OSBox core."
 
-make_repo /usr/local/src/osbox https://github.com/jerryhopper/osbox.git
+getgitfiles /usr/local/src/osbox https://github.com/jerryhopper/osbox.git
 #update_repo /usr/local/src/osbox
 chmod +x /usr/sbin/osbox
 
 echo ". "
 echo "Updating OSBox web."
-make_repo /var/www/html/osbox https://github.com/jerryhopper/blackboxweb.git
+getgitfiles /var/www/html/osbox https://github.com/jerryhopper/blackboxweb.git
 #update_repo /var/www/html/osbox
 
 echo ". "
