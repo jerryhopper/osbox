9e059ef86829105b7ff2770c4666419c9b88db8f
jerryhopper <hopper.jerry@gmail.com>
2020-03-18 18:46:46 +0100

    Update boot_command.sh

--- usr/lib/osbox/stage/boot_command.sh
+++ usr/lib/osbox/stage/boot_command.sh
@@ -237,7 +237,7 @@ fi
 if [ "$OSBOX_STATE" == "10" ]; then
     netType=$(grep "iface eth0" /etc/network/interfaces|awk '{print $4}')
     if [ "$netType" == "static" ]; then
-      echo "11" > $OSBOX_STATE_FILE
+      echo "11">$OSBOX_STATE_FILE
       OSBOX_STATE=11
     fi
 fi
