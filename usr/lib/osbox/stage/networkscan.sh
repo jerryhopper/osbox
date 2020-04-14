c72ed1ee0d32a1ef0f777dc95979136834918bfa
jerryhopper <hopper.jerry@gmail.com>
2020-04-14 17:09:13 +0200

    Update networkscan.sh

--- usr/lib/osbox/stage/networkscan.sh
+++ usr/lib/osbox/stage/networkscan.sh
@@ -10,7 +10,7 @@ fi
 find_IPv4_information
 
 
-if [ "$1" == "currentxml" ]; then
+if [ "$1" == "xml" ]; then
   nmap -sn $IPV4_ADDRESS -oX -
 else
   nmap -sn $IPV4_ADDRESS -oG -|grep Host|awk '{print $2}'
